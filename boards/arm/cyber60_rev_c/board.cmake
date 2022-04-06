d898d7a00faabea45041afab0c205261c2c83f0d
4pplet <mail@4pplet.com>
2022-04-06 23:10:44 +0200

    cont

--- boards/arm/cyber60_rev_c/board.cmake
+++ boards/arm/cyber60_rev_c/board.cmake
@@ -1,4 +1,7 @@
-# SPDX-License-Identifier: MIT
+#Copyright (c) 2022 4pplet
+#
+#SPDX-License-Identifier: MIT
+#
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
